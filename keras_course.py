01fe53588eed3885a3caa18a245af022fe069242
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2022-02-07 10:08:24 -0500

    Add Confusion matrix

--- keras_course.py
+++ keras_course.py
@@ -181,3 +181,59 @@ model.fit(x=train_samples,
           shuffle=True,
           verbose=2
 )
+
+predictions = model.predict(x=test_samples, batch_size=10, verbose=1)
+
+for i in predictions:
+    print(i)
+
+rounded_predictions = np.argmax(predictions, axis=-1)
+
+for i in rounded_predictions:
+    print(i)
+
+"""# Confusion Matrix"""
+
+# Commented out IPython magic to ensure Python compatibility.
+# %matplotlib inline
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+cm_plot_labels = ['quartile 1','quartile 2', 'quartile 3', 'quartile 4']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
